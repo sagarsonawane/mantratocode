c4d5adc991d9b5bd2d9a997e58f77db1f5e5d259
Sebastiaan Lubbers <github@duikmeester.nl>
2016-07-29 11:50:59 +0200

    Uncompress/remove zip and add VS default .gitattributes and .gitignore

---
+++ CodeSamples/WCFConfigBasedActivation/ConfigBasedService/MessageListenerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace ConfigBasedService
+{
+    [ServiceContract]
+    public class MessageListenerService : IMessageListenerService
+    {
+        [OperationContract]
+        public bool ReceiveMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return false;
+
+            return true;
+        }
+    }
+}
