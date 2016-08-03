01a4155d70f8cc1f249ca9aa882453b9e66d8f11
sagarsonawane <sagar.snwn@gmail.com>
2016-08-03 21:07:57 +0530

    A demo showing how to use Dispatcher object to achieve multi threading and responsive UI in WPF

---
+++ CodeSamples/DispatcherDemo/DispatcherDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DispatcherDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void CalculateTimeElapsed();
+        DateTime currentDateTime;
+        static int count = 0;
+
+        private void btnLong_Click(object sender, RoutedEventArgs e)
+        {
+            btnLong.IsEnabled = false;
+            currentDateTime = DateTime.Now;
+
+            Thread longRunningThread = new Thread(new ThreadStart(delegate () { Thread.Sleep(10000); Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new CalculateTimeElapsed(PostLongRunningTasks)); }));
+            longRunningThread.Start();
+        }
+
+        private void PostLongRunningTasks()
+        {
+            txtInfo.Text = "Total time elapsed required to finish the task is : " + (DateTime.Now - currentDateTime);
+            btnLong.IsEnabled = true;
+        }
+
+        private void btnCurrent_Click(object sender, RoutedEventArgs e)
+        {
+            txtCurrent.Text = "Above button is clicked " + ++count + " times";
+        }
+    }
+}
