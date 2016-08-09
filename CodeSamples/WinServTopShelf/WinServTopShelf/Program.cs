af2d4c4d6bd49fcb21bddbef705eb58558a06afe
sagarsonawane <sagar.snwn@gmail.com>
2016-08-09 18:38:23 +0530

    A code sample demonstrating how to use Topshelf open framework to create a window service

---
+++ CodeSamples/WinServTopShelf/WinServTopShelf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WinServTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(r =>
+            {
+                r.Service<WinTopShelf>
+                          (service =>
+                                  {
+                                      service.ConstructUsing(instance => new WinTopShelf());
+                                      service.WhenStarted(instance => instance.Start());
+                                      service.WhenStopped(instance => instance.Stop());
+                                  });
+
+
+                r.RunAsLocalService();
+
+                r.SetDisplayName("Topshelf facilitated service");
+
+                r.SetDescription("This service has been created using a TopShelf open source framework for creating a windows serviecs.");
+            }
+            );
+
+        }
+    }
+}
---
+++ CodeSamples/WinServTopShelf/WinServTopShelf/WinTopShelf.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf;
+
+namespace WinServTopShelf
+{
+    public class WinTopShelf
+    {
+        Timer timer;
+
+        public WinTopShelf()
+        {
+            timer = new Timer();            
+        }
+
+        private void DoWork(object sender, ElapsedEventArgs e)
+        {
+            using (var writer = new StreamWriter(@"C:\TopShelf\task.txt", true))
+            {
+                writer.WriteLine(DateTime.Now + " Topshelf has made windows service development very easy");
+            }
+        }
+
+        public void Start()
+        {
+            timer.Interval = 60000;
+            timer.Elapsed += new ElapsedEventHandler(DoWork);
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }        
+    }
+}
